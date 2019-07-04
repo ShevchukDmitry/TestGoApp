da38e7a3915eab529ab06eecee7ca4a9d9c742bc
Dmitry Shevchuk <shevchuk_dmitry@hotmail.com>
2019-07-04 12:41:44 +0300

    Vars*Slices*Func

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello from Go!")
+func main(){
+
+// create variables
+var one string 
+one = "One" // kyeword var + type
+two := "Two" // 
+three := "Three"
+var four = "Four" // keyword + init
+
+fmt.Printf("%s %s %s %s \n", one, two, three, four)
+fmt.Println(RetNums(one))
+
+// slice
+nums := []string{one,two,three,four}
+fmt.Println(nums)
+nums = append(nums, "Five")
+fmt.Println(nums)
+
+for t, nums := range nums {
+	fmt.Println(t, nums)
+	}
 }
+
+func RetNums(num string) string{
+	return "We have: "+num
+}
+
+
